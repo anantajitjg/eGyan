b9f49bf40f3ae5ad827b73b2d051dee42233db1d
anantajitjg <anantajitjg2010@gmail.com>
2017-06-07 19:48:15 +0530

    Initial Screen

---
+++ app/src/public/js/homepage.js
@@ -0,0 +1,16 @@
+$(function () {
+    // fix menu when passed
+    $('.masthead')
+        .visibility({
+            once: false,
+            onBottomPassed: function () {
+                $('.fixed.menu').transition('fade in');
+            },
+            onBottomPassedReverse: function () {
+                $('.fixed.menu').transition('fade out');
+            }
+        });
+    // Accordion
+    $('.ui.accordion')
+        .accordion();
+});
--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 
-app.get('/', function (req, res) {
-    res.send("Welcome to eGyan");
+app.get('/',function(req,res){
+   res.sendFile(path.join(__dirname, 'public', "index.html"));
 });
+
+//loading static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8080, function () {
   console.log('egyan app listening on port 8080!');
