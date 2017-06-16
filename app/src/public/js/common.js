5729f2c5a1da37e62c99719990a42f00107b87e0
anantajitjg <anantajitjg2010@gmail.com>
2017-06-16 15:21:13 +0530

    project url changed

--- app/src/public/js/common.js
+++ app/src/public/js/common.js
@@ -1,5 +1,8 @@
 var rootURL = window.location.protocol + "//" + window.location.host;
-var auth_url = "https://auth.festival40.hasura-app.io";
-var data_url = "https://data.festival40.hasura-app.io";
-var app_url = "https://app.festival40.hasura-app.io";
+//var auth_url = "https://auth.festival40.hasura-app.io";
+//var data_url = "https://data.festival40.hasura-app.io";
+//var app_url = "https://app.festival40.hasura-app.io";
+var auth_url = "https://auth.airsick30.hasura-app.io";
+var data_url = "https://data.airsick30.hasura-app.io";
+var app_url = "https://app.airsick30.hasura-app.io";
 console.log(rootURL);
