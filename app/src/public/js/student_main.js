f5073e6e3548b57ffdc496ba64a6bba6e275688d
anantajitjg <anantajitjg2010@gmail.com>
2017-06-20 00:55:35 +0530

    testing admin authorization

--- app/src/public/js/student_main.js
+++ app/src/public/js/student_main.js
@@ -1,10 +1,10 @@
-function displayName(user_id) {
+function displayUserInfo(user_id) {
     var fetch_name_query = {
         "type": "select",
         "args": {
             "table": "user_other_details",
             "columns": [
-                "user_id", "name"
+                "user_id", "name", "points"
             ],
             "where": {
                 "user_id": user_id
@@ -18,9 +18,96 @@ function displayName(user_id) {
         contentType: "application/json"
     }).done(function (data) {
         //console.log(data);
-        $("#displayName").css("display", "none").html(data[0].name).fadeIn();
+        $("#displayName").html(data[0].name).fadeIn();
+        $("#total_points").html(data[0].points + "pts").fadeIn();
+        displayBadges(user_id, data[0].points);
     }).fail(function (xhr) {
         console.log(xhr.responseText);
+    });
+}
+
+function insertBadges(badge_arr) {
+    if (badge_arr.length > 0) {
+        var insert_badge_query = {
+            "type": "insert",
+            "args": {
+                "table": "badge_status",
+                "objects": badge_arr
+            }
+        };
+        $.ajax({
+            method: "POST",
+            url: data_url + "/v1/query",
+            data: JSON.stringify(insert_badge_query),
+            contentType: "application/json"
+        }).done(function (data) {
+            console.log(data);
+        }).fail(function (xhr) {
+            console.log(xhr.responseText);
+        });
+    }
+}
+
+function displayBadges(user_id, points) {
+    var fetch_badge_query = {
+        "type": "select",
+        "args": {
+            "table": "badge_details",
+            "columns": [
+                "badge_id", "name", "points", "badge_logo",
+                {
+                    "name": "user_badge_status",
+                    "columns": ["display_status"],
+                    "where": {
+                        "user_id": user_id
+                    }
+                }
+            ],
+            "where": {
+                "points": {
+                    "$lte": points
+                }
+            },
+            "order_by": "-points"
+        }
+    };
+    $.ajax({
+        method: "POST",
+        url: data_url + "/v1/query",
+        data: JSON.stringify(fetch_badge_query),
+        contentType: "application/json"
+    }).done(function (data) {
+        //console.log(data);
+        var total_badges = data.length;
+        if (total_badges > 0) {
+            var badge_msg_content = "",
+                badge_content = "";
+            var user_badge_arr = [];
+            var badge_info = {};
+            for (var i = 0; i < total_badges; i++) {
+                if (data[i].user_badge_status.length === 0) {
+                    badge_info.user_id = user_id;
+                    badge_info.badge_id = data[i].badge_id;
+                    user_badge_arr.push(badge_info);
+                    badge_msg_content += "<div class='ui positive message'><i class='close icon'></i><div class='header'>Congratulations!</div><p>You have received <b>" + data[i].name + "!</b></p></div>";
+                }
+            }
+            for (var i = total_badges - 1; i >= 0; i--) {
+                var points_display = data[i].points > 0 ? "<div class='ui center aligned'><span class='ui grey label'>" + data[i].points + "pts</span></div>" : "";
+                badge_content += "<div class='card'><div class='image'><img class='ui image' src='/img/" + data[i].badge_logo + "'></div><div class='content'><div class='ui center aligned tiny header'>" + data[i].name + "</div>" + points_display + "</div></div>";
+            }
+            $("#user_badge_message").css("display", "none").html(badge_msg_content).fadeIn();
+            $("#badge").css("display", "none").html(badge_content).fadeIn();
+            $(".message .close").on('click', function () {
+                $(this).closest('.message').transition('fade');
+            });
+            insertBadges(user_badge_arr);
+        }
+    }).fail(function (xhr) {
+        //console.log(xhr.responseText);
+        $("#badge").css("display", "none").html("<div class='ui negative message'><div class='ui header'>Error occured!</div><div>Please refresh this page or come back later!</div></div>").fadeIn();
+    }).always(function () {
+        $("#badge .loader").hide();
     });
 }
 
@@ -88,6 +175,7 @@ function displayCourses(user_id) {
         $(".course_loader").hide();
     });
 }
+
 $(function () {
     //get user info
     $.ajax({
@@ -96,7 +184,7 @@ $(function () {
         contentType: "application/json"
     }).done(function (res) {
         //console.log(res);
-        displayName(res.hasura_id);
+        displayUserInfo(res.hasura_id);
         displayCourses(res.hasura_id);
     }).fail(function (xhr) {
         //console.log(xhr.responseText);
--- app/src/server.js
+++ app/src/server.js
@@ -6,8 +6,24 @@ var request = require('request');
 var app = express();
 app.use(bodyParser.json());
 
-var auth_url = "https://auth.festival40.hasura-app.io";
-var data_url = "https://data.festival40.hasura-app.io";
+var env = "dev";
+var auth_url = "";
+var data_url = "";
+var headers = {
+  'Content-Type': 'application/json'
+};
+
+if (env === "dev") {
+  headers.Authorization = 'Bearer ' + process.env.ADMIN_TOKEN;
+  auth_url = "https://auth.festival40.hasura-app.io";
+  data_url = "https://data.festival40.hasura-app.io";
+} else {
+  auth_url = "http://auth.hasura/";
+  data_url = "http://data.hasura";
+}
+
+headers['X-Hasura-Role'] = 'admin';
+headers['X-Hasura-User-Id'] = 1;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', "index.html"));
@@ -22,14 +38,81 @@ app.get('/logout', function (req, res) {
   res.sendFile(path.join(__dirname, 'template', "logout.html"));
 });
 
+// app.post("/signup", function (req, res) {
+//   var request_url = auth_url + '/signup';
+//   var data_query_url = data_url + "/v1/query";
+//   var user_dtl = {
+//     username: "",
+//     email: "",
+//     password: ""
+//   };
+//   var name = req.body.name;
+//   var email = req.body.email;
+//   var password = req.body.password;
+//   if (name.trim() === "" || email.trim() == "" || password.trim() === "") {
+//     res.status(400).send("Invalid input values!");
+//   } else {
+//     user_dtl.username = email;
+//     user_dtl.email = email;
+//     user_dtl.password = password;
+//     //Making HTTP request
+//     request({
+//       url: request_url,
+//       method: "POST",
+//       json: true,
+//       body: user_dtl
+//     }, function (error, response, body) {
+//       if (error) {
+//         return res.status(500).send(error.toString());
+//       }
+//       if (response.statusCode == 200) {
+//         var user_id = response.body.hasura_id;
+//         var auth = "Bearer " + response.body.auth_token;
+//         var data_query = {
+//           "type": "insert",
+//           "args": {
+//             "table": "user_other_details",
+//             "objects": [{
+//               "user_id": user_id,
+//               "name": name
+//             }]
+//           }
+//         };
+//         request({
+//           url: data_query_url,
+//           method: "POST",
+//           json: true,
+//           headers: {
+//             'Authorization': auth
+//           },
+//           body: data_query
+//         }, function (error, response, body) {
+//           if (response.body.affected_rows >= 1) {
+//             res.status(200).send("Successfully Registered!");
+//           } else {
+//             res.status(500).send(JSON.stringify({
+//               message: "There was some problem registering!"
+//             }));
+//           }
+//         });
+//       } else {
+//         if (response.statusCode == 409) {
+//           res.status(409).send(JSON.stringify({
+//             message: "Email already exists!"
+//           }));
+//         } else {
+//           res.status(response.statusCode).send(JSON.stringify(response.body));
+//         }
+//       }
+//     });
+//   }
+// });
+
+
 app.post("/signup", function (req, res) {
   var request_url = auth_url + '/signup';
   var data_query_url = data_url + "/v1/query";
-  var user_dtl = {
-    username: "",
-    email: "",
-    password: ""
-  };
+  var user_dtl = {};
   var name = req.body.name;
   var email = req.body.email;
   var password = req.body.password;
@@ -51,7 +134,7 @@ app.post("/signup", function (req, res) {
       }
       if (response.statusCode == 200) {
         var user_id = response.body.hasura_id;
-        var auth = "Bearer " + response.body.auth_token;
+        //var auth = "Bearer " + response.body.auth_token;
         var data_query = {
           "type": "insert",
           "args": {
@@ -65,10 +148,7 @@ app.post("/signup", function (req, res) {
         request({
           url: data_query_url,
           method: "POST",
-          json: true,
-          headers: {
-            'Authorization': auth
-          },
+          headers: headers,
           body: data_query
         }, function (error, response, body) {
           if (response.body.affected_rows >= 1) {
@@ -91,7 +171,6 @@ app.post("/signup", function (req, res) {
     });
   }
 });
-
 app.get('/getinfo', function (req, res) {
   var result = [];
   var info = {
